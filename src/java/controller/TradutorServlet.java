cfc8e7361596cc87f50517822de2826a6c3e0101
moura1001 <gmouraneto01@gmail.com>
2024-03-01 18:44:10 -0300

    feat: camada controller com versão inicial do servlet para gerenciamento das requisições

---
+++ src/java/controller/TradutorServlet.java
@@ -0,0 +1,40 @@
+package controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/traduzir")
+public class TradutorServlet extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+        
+        String palavra = request.getParameter("palavra");
+        String traducao = "Tradução: " + palavra;
+        
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Duoulingou</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>" + traducao + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+}
